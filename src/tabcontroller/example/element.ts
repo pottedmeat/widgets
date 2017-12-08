aa31915d3411d531df3e9d480da51bc0616549e2
Neil Roberts <neil@onebahamas.com>
2017-12-08 16:38:28 -0600

    Add custom element example

---
+++ src/tabcontroller/example/element.ts
@@ -0,0 +1,6 @@
+import registerCustomElement from '@dojo/widget-core/registerCustomElement';
+import createTabElement from '../../tabcontroller/createTabElement';
+import createTabControllerElement from '../../tabcontroller/createTabControllerElement';
+
+registerCustomElement(createTabElement);
+registerCustomElement(createTabControllerElement);
